bdbef5bbb0af558db07790c7c07da06c7a41a94b
Iman Olabode-Bello <imanolabodebello8@gmail.com>
2025-10-01 23:08:13 +0100

    editted this page to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Header from '../src/Components/Header'
+import HomePage from '../src/Pages/Home'
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<HomePage />} />
+   </Routes>
+   </BrowserRouter>
   )
 }
 

0b9c6fcc4e3bcecc3fd03f720348680d6ea1985e
Iman Olabode-Bello <imanolabodebello8@gmail.com>
2025-10-02 00:14:58 +0100

    some new tags and placement

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
-import '../Components/Header.css'
+import React, { useState } from 'react';
+import './Header.css';
+import LogoIcon from '../assets/IconLogo.png';
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className='Header-Container'>
-        <div className="Header-Wrapper">
-            
+      <div className="Header-Wrapper">
+        {/* Logo */}
+        <div className="Logo-Container">
+          <img src={LogoIcon} alt="LogoIcon" />
+          <h2>EMANN.CO</h2>
         </div>
-    </div>
-  )
-}
 
-export default Header
+        {/* Nav + Button */}
+        <div className={`Nav-Menu ${isOpen ? "open" : ""}`}>
+          <div className="Nav-Container">
+            <Link to="/" onClick={() => setIsOpen(false)}>HOME</Link>
+            <Link to="/" onClick={() => setIsOpen(false)}>All Doctors</Link>
+            <Link to="/" onClick={() => setIsOpen(false)}>ABOUT</Link>
+            <Link to="/" onClick={() => setIsOpen(false)}>CONTACT</Link>
+          </div>
+          <button className='Create-Account-Btn'>Create Account</button>
+        </div>
+
+        {/* Menu Button */}
+        <div className="Menu-Button" onClick={toggleMenu}>
+          {isOpen ? "✖" : "☰"}
+        </div>
+      </div>
+
+      {/* Overlay background */}
+      {isOpen && <div className="Overlay" onClick={toggleMenu}></div>}
+    </div>
+  );
+};
+
+export default Header;

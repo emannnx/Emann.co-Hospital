b9dec294d7cdd1a9f4f0c8550782f9b90f4b7a12
Iman Olabode-Bello <imanolabodebello8@gmail.com>
2025-10-02 11:19:49 +0100

    added somethign..dctor page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../src/Components/Header'
 import HomePage from '../src/Pages/Home'
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import AllDoctors from '../src/Pages/AllDoctors'
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
    <BrowserRouter>
    <Routes>
     <Route path='/' element={<HomePage />} />
+    <Route path='/AllDoctors' element={<AllDoctors />} />
    </Routes>
    </BrowserRouter>
   )
